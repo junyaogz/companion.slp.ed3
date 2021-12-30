536cb80c522bff8d8ad3924de1b0282c5720b141
Jun Yao <jun.yao@live.com>
2021-12-30 16:37:52 +0800

    Update and rename test.js to regex.js

---
+++ ch01/regex.js
@@ -0,0 +1,20 @@
+
+//exact match
+var myRe = /woodchucks/;
+var myArray = myRe.exec('interesting links to woodchucks and lemurs');
+console.log(myArray);
+
+//disjunctions
+var myRe = /[wW]oodchucks/;
+var myArray = myRe.exec('Woodchucks');
+console.log(myArray);
+
+//specify a range
+var myRe = /[C-G]/;
+var myArray = myRe.exec('we should call it Drenched Blossoms');
+console.log(myArray);
+
+
+
+//references；
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
--- ch01/test.js
+++
@@ -1,4 +0,0 @@
-# regular expressions
-var myRe = /woodchucks/;
-var myArray = myRe.exec('“interesting links to woodchucks and lemurs');
-console.log(myArray);
